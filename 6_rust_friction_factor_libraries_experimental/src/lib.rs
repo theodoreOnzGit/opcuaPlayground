c29f94dca921c066c909d70c8a0334c8b6b8cd69
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2022-09-12 17:12:44 -0700

    added moody and fldk code translated from csharp

---
+++ 6_rust_friction_factor_libraries_experimental/src/lib.rs
@@ -0,0 +1,145 @@
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn add_four_rust(x: f64) -> PyResult<f64>{
+    return Ok(x + 4.0);
+}
+
+#[pyfunction]
+fn fldk_rust(reynolds_number: f64, roughness_ratio: f64,
+             length_to_diameter: f64,
+             k: f64) -> PyResult<f64> {
+    return Ok(fLDK(reynolds_number, roughness_ratio, length_to_diameter, k));
+}
+
+#[pyfunction]
+fn moody_rust(reynolds_number: f64,
+         roughness_ratio: f64) -> PyResult<f64> {
+    return Ok(moody(reynolds_number, roughness_ratio));
+}
+
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn rust_functions_in_python(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(add_four_rust, m)?)?;
+    Ok(())
+}
+
+
+// here are the functions used for friction factor, rather messy but
+// for fast prototyping and sandboxing don't really care too much
+//
+
+// first, to allow non snake case names..
+#[allow(non_snake_case)]
+fn B(Re: f64) -> f64 {
+    let numerator = 37530.0_f64.powf(16.0);
+    let denominator = Re.powf(16.0);
+    return numerator/denominator;
+}
+
+#[allow(non_snake_case)]
+fn A(Re: f64, roughnessRatio: f64) -> f64 {
+    let seven_over_Re = 7.0/Re;
+    let reynolds_term = seven_over_Re.powf(0.9);
+
+    let roughness_term = 0.27 * roughnessRatio;
+
+    let log_fraction = 1.0/(reynolds_term + roughness_term);
+    // we will need natural logarithms:
+    let inner_bracket_term = 2.457*log_fraction.ln();
+
+    let A = inner_bracket_term.powf(16.0);
+
+    return A;
+
+
+}
+
+#[allow(non_snake_case)]
+fn churchillInnerTerm(Re: f64, roughnessRatio: f64) -> f64 {
+
+    let eight_over_Re = 8.0/Re;
+    let laminarTerm = eight_over_Re.powf(12.0);
+
+    let Aterm = A(Re,roughnessRatio);
+    let Bterm = B(Re);
+
+    let APlusBInverse = 1.0/(Aterm+Bterm);
+    let turbulentTerm = APlusBInverse.powf(3.0/2.0);
+
+    return laminarTerm + turbulentTerm;
+}
+
+// this particular implementation uses the churchill correlation
+#[allow(non_snake_case)]
+fn fanning(ReynoldsNumber: f64, roughnessRatio: f64) -> f64{
+
+    if ReynoldsNumber == 0.0 {
+        panic!("Re = 0.0");
+    }
+
+    if ReynoldsNumber < 0.0 {
+        panic!("Re<0.0");
+    }
+
+    if roughnessRatio < 0.0 {
+        panic!("roughnessRatio<0.0");
+    }
+
+    let innerTerm = churchillInnerTerm(ReynoldsNumber, roughnessRatio);
+    let powerTerm = innerTerm.powf(1.0/12.0);
+    let fanningFrictionFactor = 2.0 * powerTerm;
+    return fanningFrictionFactor;
+}
+
+#[allow(non_snake_case)]
+fn darcy(ReynoldsNumber: f64, roughnessRatio: f64) -> f64 {
+    return 4.0*fanning(ReynoldsNumber, roughnessRatio);
+}
+
+#[allow(non_snake_case)]
+fn moody(ReynoldsNumber: f64, roughnessRatio: f64) -> f64 {
+    return 4.0*fanning(ReynoldsNumber, roughnessRatio);
+}
+
+
+#[allow(non_snake_case)]
+fn fLDK(ReynoldsNumber: f64,
+                   roughnessRatio: f64,
+                   lengthToDiameterRatio: f64,
+                   K: f64) -> f64{
+    if ReynoldsNumber == 0.0 {
+        panic!("Re = 0");
+    }
+
+    if ReynoldsNumber < 0.0 {
+        panic!("Re < 0");
+    }
+
+    if roughnessRatio < 0.0 {
+        panic!("roughnessRatio<0.0");
+    }
+
+    if lengthToDiameterRatio <= 0.0 {
+        panic!("lengthToDiameterRatio<=0.0");
+    }
+
+    if K < 0.0 {
+        panic!("For m loss coefficient K < 0.0");
+    }
+
+    let f = darcy(ReynoldsNumber, roughnessRatio);
+    let fLDK = f*lengthToDiameterRatio + K;
+
+    return fLDK;
+}
